088e8f49918baf8309e0b5d8c91a4089af32ede4
jpmlyr <jpmlyr@gmail.com>
2016-05-27 23:47:42 +0900

    20160527연습

--- 20160526.py
+++ 20160526.py
@@ -5,3 +5,13 @@ def f(**args):
 
 f(a=1,b=2)
 
+def my_func(param):
+	param = "ff"
+	print(param)
+	print(id(param))
+	print("-"*10)
+
+param = '11'
+print(id(param))
+my_func(param)
+print(id(param))
