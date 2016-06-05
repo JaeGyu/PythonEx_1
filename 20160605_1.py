434233da48409c362964f98b6c3f64d00cfc5d94
jpmlyr <jpmlyr@gmail.com>
2016-06-05 16:16:00 +0900

    20160605실습

--- 20160605_1.py
+++ 20160605_1.py
@@ -1,1 +1,16 @@
 #_*_ coding: utf-8 _*_
+
+
+class S1:
+	a = 1 #a라는 변수는 S1이라는 클래스의 이름공간에 존재한다.
+
+print S1.a
+print 
+
+S1.b = 2 #클래스 이름 공간에 새로운 이름의 생성
+print S1.b
+print
+
+print dir(S1) #S1에 포함된 이름들을 리스트로 반환
+del S1.b #이름 공간 S1에서 b삭제
+print dir(S1)
