fc5b42e7f23a017f2ec5d5d4242bb8721d3f8280
jpmlyr <jpmlyr@gmail.com>
2016-06-27 23:33:16 +0900

    .

--- 20160625.py
+++ 20160625.py
@@ -1,4 +1,5 @@
 #_*_ coding: utf-8 _*_
+from __future__ import division
 
 print "d"
 
@@ -9,4 +10,19 @@ for i in [1,2,3,4,5]:
 		print i+j
 	print i
 print "done looping"
+
+
+
+list_of_lists = [[1,2,3],
+                        [4,5,6],
+						[7,8,9]]
+
+print list_of_lists
+
+import re
+my_regex = re.compile("[0-9]+",re.I)
+
+
+print 5/2
+print 5//2
 
