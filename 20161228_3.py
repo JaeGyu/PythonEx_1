d0301141e5efb657934b301023f40751f83a9a61
jaegyu <jpmlyr@gmail.com>
2016-12-29 07:56:02 +0900

    .

--- 20161228_3.py
+++ 20161228_3.py
@@ -1,10 +1,18 @@
 from flask import Flask
+import json
 
 app = Flask(__name__)
 
 @app.route("/")
 def helloworld():
     return "Hello"
+
+@app.route("/member", methods=["GET"])
+def getMember():
+    result = []
+    mem = {"name":"alice", "age":27}
+    result.append(mem)
+    return json.dumps(result)
 
 def main():
     app.run(debug=True, host="0.0.0.0", port=5009)
