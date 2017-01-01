18426841b4fa9b39938b37bbe2de9546493691f7
jaegyu <jpmlyr@gmail.com>
2017-01-02 08:14:31 +0900

    .

---
+++ 20170102.py
@@ -0,0 +1,10 @@
+#_*_ coding: UTF-8 _*_
+import tkinter
+
+print("GUI 실행 입니다.")
+root = tkinter.Tk()
+frame = tkinter.Frame(root, height = 100, width = 100, relief = 'sunken', bd=2, bg= '#D9E5FF')
+frame.pack()
+bt = tkinter.Button()
+root.mainloop()
+
