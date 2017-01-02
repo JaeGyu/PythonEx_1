6ff0a9ce28e3896d250bf78046256dd2872052c7
jaegyu <jpmlyr@gmail.com>
2017-01-02 10:22:20 +0900

    .

---
+++ 20170102_1.py
@@ -0,0 +1,22 @@
+def func_name():
+    print("call func")
+
+print(func_name)
+func_name()
+
+def progression(n, step=1):
+    x = 1
+    while x<=n:
+        print(x)
+        x+=step
+    
+progression(10)
+
+def while_test():
+    x = 0
+    while x<10:
+        print(x)
+        x += 1
+
+while_test()
+
