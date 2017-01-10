689df2a809c056d003361bcc6683b0a0065b30f2
jaegyu <jpmlyr@gmail.com>
2017-01-11 08:58:40 +0900

    .

--- 20170111.py
+++ 20170111.py
@@ -12,4 +12,36 @@ if m:
     print("매치 됨 : ", m.group())
 else:
     print("매치 안됨")
-    
+
+p = re.compile("[abc]")
+print(p.match("a"))
+print(p.match("before"))
+print(p.match("dude"))
+
+print("-" * 60)
+p = re.compile("[1-3]")
+print(p.match("aa"))
+print(p.match("345"))
+print("-" * 60)
+
+p = re.compile("[^0-9]")
+print(p.match("123"))
+print(p.match("for"))
+print("-"*60)
+
+p=re.compile("[\d]") #숫자
+print(p.match("123"))
+print("-"*60)
+
+p = re.compile("[\D]") #숫자가 아닌거
+print(p.match("123"))
+print("-"*60)
+
+p = re.compile("a.b")
+print(p.match("a123b"))
+print("-"*60)
+
+p = re.compile("a.b")
+print(p.match("asbc"))
+print("-"*60)
+
