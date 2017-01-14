dbd5ce1647924cbe60ad62c24b0d7d599450030a
jaegyu <jpmlyr@gmail.com>
2017-01-15 08:53:06 +0900

    .

--- 20170114/20170114.py
+++ 20170114/20170114.py
@@ -1,9 +1,16 @@
 print("-"*60)
 
-
 def main():
     print("hello")
 
+    v = [i for i in range(10)]
+    print(v)
+
+    v = [lambda x : x+1 for i in range(10)]
+    print(v)
+
 if __name__ == "__main__":
     main()
+
+
 
