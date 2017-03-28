b9ef8bf7af8b6322e3cdf7558102cfa367a52bf1
icpu <icpu@naver.com>
2017-03-29 07:41:49 +0900

    .

---
+++ QuickSort_Ex2.py
@@ -0,0 +1,16 @@
+import random as rnd  
+
+
+def quick_sort(list):
+    pivot_idx = len(list) - 1
+    
+    return list
+
+def main():
+    list = rnd.sample(range(10), 10)
+    print("Before sort : ", list)
+    print("After sort  : ",list)
+
+
+if __name__ == "__main__":
+    main()
