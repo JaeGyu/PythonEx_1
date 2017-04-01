03b80d6b118e146fbf7347a89e5e66480a3e7403
icpu <icpu@naver.com>
2017-04-02 08:27:44 +0900

    파이썬 200제 연습

---
+++ py200_032.py
@@ -0,0 +1,15 @@
+'''
+시퀀스 자료에 어떤 값이 있는지 없는지를 체크 한다
+
+'''
+
+list_data = [1, 2, 3, 4]
+ret1 = 5 in list_data
+ret2 = 4 in list_data
+print(ret1, ret2)
+str = "abcde"
+
+ret3 = 'c' in str
+ret4 = 'l' in str
+
+print(ret3, ret4)
---
+++ py200_033.py
@@ -0,0 +1,15 @@
+str1 = "나는 파이썬 프로그래머다"
+str2 = 'I am python programmer'
+str3 = '''I love
+    Python.{} You love
+Python too!
+'''
+str4 = """나는 파이썬을
+    좋아합니다
+"""
+
+print(str1)
+print(str2)
+print(str3.format("이부분은?"))
+print(str4)
+
