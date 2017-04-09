f6021bad0bdaad9f43195f0391d987f6c6b7651f
icpu <icpu@naver.com>
2017-04-09 18:43:51 +0900

    .

--- py200_054.py
+++ py200_054.py
@@ -1,9 +1,16 @@
 class Add:
-    def add(self, n1, n2, n3 = 0):
+    def add(self, n1, n2, n3=0):
+        print("add of Add")
         return n1 + n2 + n3
 
 
-class Calculator(Add):
+class Add2:
+    def add(self, n1, n2, n3=0):
+        print("add of Add2")
+        return n1 * n2
+
+# 부모들 중 같은 시그너쳐의 메서드가 있다면 먼저 상속선언된 부모의 메서드가 우선시 되는 것 같다
+class Calculator(Add2, Add):
     def sub(self, n1, n2):
         return n1 - n2
 
@@ -12,4 +19,4 @@ o = Calculator()
 
 print(o.add(1, 2))
 print(o.sub(1, 2))
-print(o.add(1, 2, 3))
+# print(o.add(1, 2, 3))
