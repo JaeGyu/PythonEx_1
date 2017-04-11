568d41bd1f2fe268ab0d3e141f7c714523c1f699
icpu <icpu@naver.com>
2017-04-11 11:30:34 +0900

    .

--- flaskEx/socketIOEx/app_start.py
+++ flaskEx/socketIOEx/app_start.py
@@ -1,3 +1,4 @@
-from socketIOEx import app
+from socketIOEx import app, socket_io
 if __name__ == '__main__':
-	app.run(host='0.0.0.0')
+	#app.run(host='0.0.0.0')
+	socket_io.run(app, debug = True, port=5000)
--- flaskEx/socketIOEx/socketIOEx/__init__.py
+++ flaskEx/socketIOEx/socketIOEx/__init__.py
@@ -1,8 +1,31 @@
-from flask import Flask
+from flask import Flask, jsonify, render_template
+from subprocess import call
+from flask_socketio import SocketIO, send
+
 
 app = Flask(__name__)
+app.secret_key = "mysecret"
 
+socket_io = SocketIO(app)
 
 @app.route('/')
 def socketIOEx():
 	return 'hello world'
+
+@app.route("/chat")
+def chatting():
+	return render_template("chat2.html")
+
+@socket_io.on("message")
+def request(message):
+	print("message : " + message)
+	to_client = dict()
+	if message == "new_connect":
+		to_client["message"] = "사로운 유저가 난입했다."
+		to_client["type"] = "connect"
+	else:
+		to_client["message"] = message
+		to_client["type"] = "nomal"
+	send(to_client, broadcast = True)
+
+
