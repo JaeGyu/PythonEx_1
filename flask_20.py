a4882775719b36d127cc7457704e8d9dc191d1e1
icpu <icpu@naver.com>
2017-04-28 10:28:13 +0900

    .

--- flask_20.py
+++ flask_20.py
@@ -1,24 +1,56 @@
-from flask import Flask, jsonify
-from flask_restplus import Api, Resource
-import flask_20_models as model
+from flask import Flask, jsonify, request
+from flask_restplus import Api, Resource, fields
+import flask_20_models as books
 
 app = Flask(__name__)
-api = Api(app)
 
-'''
-1) get] cars
-'''
+api = Api(app, version='1.0', title='Books API',
+    description='A simple Books API',
+)
 
-'''
-GET
-db를 이용해서 가져온다.
-'''
-@api.route("/books")
-class Cars(Resource):
+ns = api.namespace('books', description='Books operations')
+
+book = api.model("book",{
+    "name" : fields.String(required = True, description="도서명"),
+    "price" : fields.Integer(required = False, description="가격")
+})
+
+@ns.route("/")
+class Books(Resource):
     def get(self):
-        return jsonify(model.books)
+        return jsonify(books.find_all())
+    
+    @ns.expect(book)
+    # @ns.marshal_with(book, code=201)
+    def post(self):
+        book = api.payload
+        books.save(book)
+        return self.get()
+    
 
-@api.route("/cars/")
+@ns.route("/<id>")
+@ns.doc(params={"id":"도서의 아이디"})
+class Book(Resource):
+    def get(self,id):
+        return jsonify(books.find_by_id(id))
+    
+    def delete(self):
+        return []
+
+
+
+nss = api.namespace('blog/categories', description='Blog operations')
+
+@nss.route("/")
+class CategoryCollection(Resource):
+
+    def get(self):
+        return []
+    
+    @api.response(201, "Category successfully created.")
+    def post(self):
+        print(request.json)
+        return None, 201
 
 if __name__ == "__main__":
     app.run(debug = True)
--- flask_20_models.py
+++ flask_20_models.py
@@ -1,22 +1,57 @@
 from sqlite3 import * 
 
+def save(book):
+    name = book["name"]
+    price = book["price"]
+    mydb = connect("/Users/jaegyuhan/dev/sqlite3/books.db") #db파일에 연결 없다면 생성
+    csr = mydb.cursor()  #커서 객체 얻기 
+    rst = csr.execute("insert into books(name,price) values(?,?)", (name,price))
+    print("rst : ",rst)
+    mydb.commit()
+    mydb.close()
 
-mydb = connect("/Users/jaegyuhan/dev/sqlite3/books.db") #db파일에 연결 없다면 생성
-csr = mydb.cursor()  #커서 객체 얻기 
-csr.execute("""select name, 
+def find_all():
+    mydb = connect("/Users/jaegyuhan/dev/sqlite3/books.db") #db파일에 연결 없다면 생성
+    csr = mydb.cursor()  #커서 객체 얻기 
+    csr.execute("""select name, 
                       price 
                  from books""")
-        
-all_data = csr.fetchall()
-col_names = [column[0] for column in csr.description]
 
-rows = [rec for rec in all_data]
-rows_dict = map(lambda row: dict(zip(col_names, row)), all_data)
+    all_data = csr.fetchall()
+    col_names = [column[0] for column in csr.description]
 
-l = [r for r in rows_dict]
-print(l)
+    rows = [rec for rec in all_data]
+    rows_dict = map(lambda row: dict(zip(col_names, row)), all_data)
 
-mydb.commit()
-mydb.close()
+    l = [r for r in rows_dict]  
+
+    mydb.commit()
+    mydb.close()
+
+    return l
+
+def find_by_id(id):
+    mydb = connect("/Users/jaegyuhan/dev/sqlite3/books.db") #db파일에 연결 없다면 생성
+    csr = mydb.cursor()  #커서 객체 얻기 
+    csr.execute("""select name, 
+                      price 
+                 from books
+                 where id = ?
+                 """,id)
+
+    all_data = csr.fetchall()
+    col_names = [column[0] for column in csr.description]
+
+    rows = [rec for rec in all_data]
+    rows_dict = map(lambda row: dict(zip(col_names, row)), all_data)
+
+    l = [r for r in rows_dict]  
+
+    mydb.commit()
+    mydb.close()
+
+    return l
+    
+
 
     
