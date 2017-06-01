6d0daf0fdfe78eb93accfaadbeab349b9a72d159
icpu <icpu@naver.com>
2017-06-01 09:33:18 +0900

    .

--- 20170601.py
+++ 20170601.py
@@ -3,8 +3,16 @@
 def sum(a,b):
     return a+b
 
+
+def sumToMany(*args):
+    sum = 0
+    for i in args:
+        sum+=i
+    return sum
+
 def main():
     print(sum(4,5))
+    print(sumToMany(1,2,3,4,5,6,7,8,9,10))
 
 if __name__ == "__main__":
     main()
