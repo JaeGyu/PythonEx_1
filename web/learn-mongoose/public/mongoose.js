d0cf20030e16a62632e64a850aa30c355e436515
jaegyu <jpmlyr@gmail.com>
2017-08-04 13:22:54 +0900

    .

--- web/learn-mongoose/public/mongoose.js
+++ web/learn-mongoose/public/mongoose.js
@@ -1,3 +1,7 @@
+/**
+ * document.querySelectorAll의 결과는 유사 배열이다. 
+ * forEach를 사용하기 위해 Array의 프로토타임을 call 했다.
+ */
 [].forEach.call(document.querySelectorAll('#user-list tr'), function (el) {
     el.addEventListener('click', function () {
         console.log(el.querySelector('td'));  //첫번째 td를 가져 온다.
@@ -6,6 +10,48 @@
     });
 });
 
+//사용자 로딩
+function getUser() {
+    var xhr = new XMLHttpRequest();
+
+    console.log("getUser이 실행 됐어요!");
+};
+
 function getComment(id) {
     console.log(id);
 };
+
+document.getElementById('user-form').addEventListener('submit', function (e) {
+    e.preventDefault();
+    console.log("user-form 서브밋~!!");
+    var name = e.target.username.value;
+    var age = e.target.age.value;
+    var married = e.target.married.checked;
+    console.log(name, age, married);
+
+    if (!name) {
+        return alert('이름을 입력하세요');
+    }
+
+    if (!age) {
+        return alert('나이를 입력하세요');
+    }
+
+    var xhr = new XMLHttpRequest();
+
+    xhr.onload = function () {
+        if (xhr.status === 201) {
+            console.log(xhr.responseText);
+            getUser();
+        } else {
+            console.error(xhr.responseText);
+        }
+    };
+
+    xhr.open('POST', '/users');
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.send(JSON.stringify({ name: name, age: age, married: married }));
+    e.target.age.value = '';
+    e.target.married.checked = false;
+
+});
--- web/learn-mongoose/routes/users.js
+++ web/learn-mongoose/routes/users.js
@@ -1,9 +1,34 @@
 var express = require('express');
+var User = require('../schemas/user');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
+  User.find({})
+    .then((users) => {
+      res.json(users);
+    })
+    .catch((err) => {
+      console.error(err);
+      next(err);
+    });
   res.send('respond with a resource');
+});
+
+router.post('/', (req, res, next) => {
+  const user = new User({
+    name: req.body.name,
+    age: req.body.age,
+    married: req.body.married,
+  });
+
+  user.save()
+    .then((result) => {
+      console.log("저장 후 결과: ", result);
+      res.status(201).json(result);
+    }).catch((err) => {
+      console.log(err);
+      next(err);
+    });
 });
 
 module.exports = router;
