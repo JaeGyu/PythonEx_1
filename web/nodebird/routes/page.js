58a45b76f0c128d6798d9d773a7c335a412b6423
jaegyu <jpmlyr@gmail.com>
2017-08-13 08:01:57 +0900

    .

--- web/nodebird/routes/page.js
+++ web/nodebird/routes/page.js
@@ -2,15 +2,31 @@ const express = require('express');
 
 const router = express.Router();
 
+const users = {
+    Followings: [{
+        nick: 'alice'
+    }, {
+        nick: 'bob'
+    }],
+    Followers: [{
+        nick: 'hong'
+    }]
+};
+
 router.get('/profile', (req, res) => {
-    res.render('profile', { title: '내 정보 - NodeBird', user: null });
+    res.render('profile', {
+        title: '내 정보 - NodeBird',
+        user: users,
+        loginError: req.flash('loginError')
+    });
 });
 
 router.get('/join', (req, res) => {
     res.render('join', {
         title: '회원가입',
         user: null,
-        joinError: req.flash('joinError') //-->
+        joinError: req.flash('joinError'), //-->
+        loginError: req.flash('loginError')
     });
 });
 
@@ -19,6 +35,15 @@ router.get('/', (req, res, next) => {
         title: 'NodeBird',
         twits: [],
         user: null,
+        loginError: req.flash('loginError')
+    });
+});
+
+router.get('/lay', (req, res, next) => {
+    res.render('layout', {
+        title: 'NodeBird',
+        twits: [],
+        user: 'Alice',
         loginError: req.flash('loginError')
     });
 });
