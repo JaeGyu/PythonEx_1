c7cf0f9e3b205bc0f40f99babb0b41efa8683194
jaegyu <jpmlyr@gmail.com>
2017-08-14 10:36:44 +0900

    .

--- web/nodebird/sandbox.js
+++ web/nodebird/sandbox.js
@@ -3,3 +3,17 @@ const path = require('path');
 console.log(process.env.PORT);
 console.log(__dirname);
 console.log(path.join(__dirname, 'views'))
+
+const a = (b, c) => (
+    b, c
+);
+
+const b = (a, c) => {
+    return (a, c);
+};
+
+const c = (a, b);
+
+console.log(a(1, 3));
+console.log(b(1, 3));
+console.log(c(1, 3));
