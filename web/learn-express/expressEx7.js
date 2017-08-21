9704e5c0fc6a7d768b336cf8b725ca43cff56eb5
jaegyu <jpmlyr@gmail.com>
2017-08-21 20:00:11 +0900

    .

--- web/learn-express/expressEx7.js
+++ web/learn-express/expressEx7.js
@@ -1,30 +1,87 @@
 const express = require('express');
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 const app = express();
 
-const users = [{
+let users = [{
+    id: 1,
     name: 'alice',
     age: 33
 }, {
+    id: 2,
     name: '홍길동',
     age: 35
+}, {
+    id: 3,
+    name: 'bob',
+    age: 45
 }];
 
 app.use(morgan('dev'));
+app.use(bodyParser.json());  // application/json 형태를 파싱해 준다.
+app.use(bodyParser.urlencoded({ extended: true }));  //urlencoded를 파싱해 준다.
 
 app.get('/', (req, res) => {
     res.send("hello");
 });
 
-app.get('/users', (req, res) => {
-    res.json(users);
+app.get('/users', (req, res, next) => {
+    req.query.limit = req.query.limit || 10;
+    next();
+}, (req, res) => {
+    const { limit } = req.query;
+
+    if (Number.isNaN(parseInt(limit, 10))) {
+        return res.status(400).end();
+    }
+    res.json(users.slice(0, limit));
     // res.send(users);
-})
+});
+
+app.get('/users/:id', (req, res) => {
+    const id = parseInt(req.params.id, 10);
+
+    if (Number.isNaN(id)) {
+        return res.status(400).end();
+    }
+
+    const user = users.filter((user) => {
+        return user.id === id;
+    })[0];
+
+    if (user === undefined) {
+        return res.status(404).end();
+    }
+
+    res.json(user);
+});
+
+app.delete('/users/:id', (req, res) => {
+    const id = parseInt(req.params.id, 10);
+    if (Number.isNaN(id)) {
+        return res.status(400).end();
+    }
+    users = users.filter(user => user.id !== id);
+    res.status(204).end();
+});
+
+app.post('/users', (req, res) => {
+    const name = req.body.name;  //이건 bodyParser을 적용해야 사용할 수 있다.
+
+    if (!name) return res.status(400).end();
+    if (users.filter(user => user.name === name).length) {
+        return res.status(409).end();
+    }
+
+    const age = req.body.age;
+    const id = Date.now();
+    const user = { id, name, age }
+    users.push(user);
+    res.status(201).json(user);
+});
 
 app.listen(3000, () => {
     console.log("Server is Running");
 });
-
-
 
 module.exports = app;
--- web/learn-express/expressEx7.spec.js
+++ web/learn-express/expressEx7.spec.js
@@ -1,7 +1,132 @@
 const app = require('./expressEx7');
 const request = require('supertest');
+const should = require('should');
 
 /**
  * 테스트 수트를 만든다. 
  */
-describe('GET /users는 ');
+describe('GET /users는 ', () => {
+    describe('성공시', () => {
+        it('유저 객체를 담은 배열로 응답 한다.', (done) => {
+            request(app)
+                .get('/users')
+                .end((err, res) => {
+                    // console.log(res.body);
+                    res.body.should.be.instanceOf(Array);
+                    done();
+                });
+        });
+
+        it('최대 limit갯수만큼 응답 한다.', (done) => {
+            request(app)
+                .get('/users?limit=2')
+                .end((err, res) => {
+                    res.body.should.have.lengthOf(2);
+                    done();
+                });
+        });
+    });
+
+    describe('실패시', () => {
+        it('limit시 숫자형이 아니면 400을 응답한다.', (done) => {
+            request(app)
+                .get('/users?limit=two')
+                .expect(400)
+                .end(done);
+        });
+    });
+});
+
+describe('GET /users/1는 ', () => {
+    describe('성공시', () => {
+        it('id가 1인 유저 객체를 반환한다.', (done) => {
+            request(app)
+                .get('/users/1')
+                .end((err, res) => {
+                    res.body.should.have.property('id', 1);
+                    done();
+                });
+        });
+    });
+
+    describe('실패시', () => {
+        it('id가 숫자가 아닐경우 400으로 응답 한다.', (done) => {
+            request(app)
+                .get("/users/a")
+                .expect(400)
+                .end(done);
+        });
+        it('id로 유저를 찾을 수 없는 경우 404로 응답 한다.', (done) => {
+            request(app)
+                .get("/users/5")
+                .expect(404)
+                .end(done);
+        });
+    });
+
+    describe('GET /users/1', () => {
+        describe('성공시', () => {
+            it('204를 응답 한다.', (done) => {
+                request(app)
+                    .delete('/users/1')
+                    .expect(204)
+                    .end(done);
+            });
+        });
+        describe('실패시', () => {
+            it('id가 숫자가 아닐경우 400을 응답 한다.', (done) => {
+                request(app)
+                    .delete('/users/a')
+                    .expect(400)
+                    .end(done);
+            });
+        });
+    });
+
+    describe('POST /users', () => {
+        describe('성공시', () => {
+            let name = 'kobin';
+            let body;
+            before(done => {
+                request(app)
+                    .post('/users')
+                    .send({ name, age: 45 })
+                    .expect(201)
+                    .end((err, res) => {
+                        body = res.body;
+                        done();
+                    })
+            });
+
+            /**
+             * 아래는 비동기 테스트가 아니기 때문에 done 메커니즘이 필요 없다.
+             */
+            it('생성된 유저 객체를 반환 한다.', () => {
+                body.should.have.property('id');
+            });
+
+            /**
+             * 아래는 비동기 테스트가 아니기 때문에 done 메커니즘이 필요 없다.
+             */
+            it('입력한 네임을 반환 한다. ', () => {
+                body.should.have.property('name', name);
+            });
+        });
+        describe('실패시', () => {
+            it('name 파라메터를 누락시 400을 반환한다.', (done) => {
+                request(app)
+                    .post('/users')
+                    .send({})
+                    .expect(400)
+                    .end(done)
+            });
+            it("name이 중복일 경우 409를 반환 한다. ", (done) => {
+                request(app)
+                    .post('/users')
+                    .send({ name: 'bob', age: 45 })
+                    .expect(409)
+                    .end(done)
+            });
+        });
+    });
+});
